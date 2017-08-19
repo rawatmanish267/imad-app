dad2117dcfe917b2be412fa688f8681804cea599
rawatmanish267 <30626949+rawatmanish267@users.noreply.github.com>
2017-08-19 15:48:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,3 +17,18 @@ function exec(){
     request.send(); 
     console.log('EXECUTED');
 }
+//submit name
+var nameInput=document.getElementById('name');
+var name=nameInput.value;
+var submit=document.getElementById('submit_btn');
+submit.onClick=function(){
+    //make a request to server and send name
+    
+    //capture a list of names and render it as a list
+    var names=['name1','name2','name3'];
+    var list='';
+    for(var i=0;i<names.length;i++)
+    {
+        list+='<li>'+names[i]+'</li>';
+    }
+};
