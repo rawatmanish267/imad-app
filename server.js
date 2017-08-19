f556565473617104574e6afcd9163c40ffa5c2c5
rawatmanish267 <30626949+rawatmanish267@users.noreply.github.com>
2017-08-19 16:19:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -104,7 +104,7 @@ app.get('/counter',function(req, res){
 });
 
 var names=[];
-app.get('/submit-name/:name',function(req, res){
+app.get('/submit-name',function(req, res){
    //get the name from the request
     var name=req.query.name;
     names.push(name);
