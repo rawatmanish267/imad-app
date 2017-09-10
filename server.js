2f3ef0f879ce281e6a037f0c2c907df664e9a138
rawatmanish267 <30626949+rawatmanish267@users.noreply.github.com>
2017-09-10 20:17:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -111,7 +111,7 @@ app.get('/test-db',function(req,res){
       }
       else
       {
-        res.send(JSON.stringify(result));    
+        res.send(JSON.stringify(result.rows));    
       }
    });
 });
